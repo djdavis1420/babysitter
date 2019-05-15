b8b239337f410db09abea213d25bcbb92c023e6d
Dylan Davis <djdavis1420@gmail.com>
2019-05-15 12:17:37 -0500

    Added Input Validation

--- src/console_app.py
+++ src/console_app.py
@@ -2,6 +2,25 @@ from src.models.babysitter import Babysitter
 from src.models.family import Family
 from src.models.job import Job
 
+
+def validate_input(expected_type, prompt):
+    response = None
+    while type(response) is not expected_type:
+        response = input(prompt)
+        try:
+            if expected_type == int and int(response) > 0:
+                response = int(response)
+            elif expected_type == float and float(response) > 0:
+                response = float(response)
+            elif expected_type == str:
+                continue
+            else:
+                raise ValueError
+        except ValueError:
+            print('That input is not valid. Try again.')
+    return response
+
+
 print('-' * 100)
 print('Babysitter Payment Calculator')
 print('-' * 100)
@@ -10,19 +29,19 @@ print('Assuming: Rates of Pay will always be integers.')
 print('Assuming: Times will always be in 24-hour format on the hour.')
 print('-' * 100)
 
-babysitter_name = input('What is your name? ')
-babysitter_start_time = int(input('What is your start time? '))
-babysitter_end_time = int(input('What is your end time? '))
+babysitter_name = validate_input(str, 'What is your name? ')
+babysitter_start_time = validate_input(int, 'What is your start time? ')
+babysitter_end_time = validate_input(int, 'What is your end time? ')
 babysitter = Babysitter(babysitter_name, babysitter_start_time, babysitter_end_time)
 
-family_name = input('What is the client\'s family name? ')
-family_rates_of_pay = int(input('How many rates of pay does this family offer? Please enter 2 or 3. '))
-family_standard_rate = float(input('What is their standard rate of pay? '))
-family_overtime_rate = float(input('What is their overtime rate of pay? '))
-family_standard_rate_limit = int(input('At what time does standard pay end and overtime pay begin? '))
+family_name = validate_input(str, 'What is the client\'s family name? ')
+family_rates_of_pay = validate_input(int, 'How many rates of pay does this family offer? Please enter 2 or 3. ')
+family_standard_rate = validate_input(float, 'What is their standard rate of pay? ')
+family_overtime_rate = validate_input(float, 'What is their overtime rate of pay? ')
+family_standard_rate_limit = validate_input(int, 'At what time does standard pay end and overtime pay begin? ')
 if family_rates_of_pay == 3:
-    family_alternate_rate = float(input('What is their alternate rate of pay? '))
-    family_overtime_rate_limit = int(input('At what time does overtime pay end and alternate pay begin? '))
+    family_alternate_rate = validate_input(float, 'What is their alternate rate of pay? ')
+    family_overtime_rate_limit = validate_input(int, 'At what time does overtime pay end and alternate pay begin? ')
     family = Family(family_name, family_standard_rate, family_overtime_rate, family_alternate_rate)
     family.hour_schedule['standard_rate_limit'] = family_standard_rate_limit
     family.hour_schedule['overtime_rate_limit'] = family_overtime_rate_limit
@@ -30,8 +49,8 @@ else:
     family = Family(family_name, family_standard_rate, family_overtime_rate)
     family.hour_schedule['standard_rate_limit'] = family_standard_rate_limit
 
-job_start_time = int(input('What time does this job start? '))
-job_end_time = int(input('What time does this job end? '))
+job_start_time = validate_input(int, 'What time does this job start? ')
+job_end_time = validate_input(int, 'What time does this job end? ')
 job = Job(job_start_time, job_end_time)
 
 try:
